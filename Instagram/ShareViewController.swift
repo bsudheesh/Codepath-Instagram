58e1007b44d1e32d0a7ac8ed1d2266c4b3ff0455
Sudheesh Bhattarai <sudheesh.bhattarai@bison.howard.edu>
2017-03-12 21:53:35 -0400

    Adding sharing photo feature.

--- Instagram/ShareViewController.swift
+++ Instagram/ShareViewController.swift
@@ -9,8 +9,18 @@
 import UIKit
 import Parse
 
-class ShareViewController: UIViewController {
+class ShareViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    
+    @IBOutlet weak var postPhotoLabel: UIImageView!
 
+    @IBOutlet weak var captionTextFiled: UITextField!
+    @IBOutlet weak var albumButton: UIButton!
+    @IBOutlet weak var cameraButton: UIButton!
+    @IBOutlet weak var uploadButton: UIButton!
+    let picker = UIImagePickerController()
+    var image = [PFFile]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -19,19 +29,57 @@ class ShareViewController: UIViewController {
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+        
         // Dispose of any resources that can be recreated.
     }
     
-
-    @IBOutlet weak var logOut: UIButton!
     
-
-     @IBAction func logOutButton(_ sender: Any) {
-        PFUser.logOut()
-        NotificationCenter.default.post(name: NSNotification.Name(rawValue: "UserDidLogOut"), object: nil)
+    
+    @IBAction func albumButtonClicked(_ sender: Any) {
+        let vc = UIImagePickerController()
+        vc.delegate = self
+        vc.allowsEditing = true
+        vc.sourceType = .photoLibrary
+        present(vc, animated: true, completion: nil)
+    }
+    
+    
+    @IBAction func cameraButtonClick(_ sender: Any) {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.sourceType = .camera
         
-     }
+        present(picker, animated:true, completion: nil)
+    }
     
+    func imagePickerController(_ picker: UIImagePickerController,
+                               didFinishPickingMediaWithInfo info: [String : Any]) {
+        // Get the image captured by the UIImagePickerController
+        let originalImage = info[UIImagePickerControllerOriginalImage] as! UIImage
+        let editedImage = info[UIImagePickerControllerEditedImage] as! UIImage
+        postPhotoLabel.image = editedImage
+        // Do something with the images (based on your use case)
+        
+        // Dismiss UIImagePickerController to go back to your original view controller
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func uploadButtonClicked(_ sender: Any) {
+        
+        Post.postUserImage(image: postPhotoLabel.image,
+                           withCaption: captionTextFiled.text,
+                           withCompletion: { _ in
+                            DispatchQueue.main.async {
+                                self.postPhotoLabel.image = nil
+                                self.captionTextFiled.text = nil
+                                
+                            }}
+        )
+       
+        //Post.postUserImage(image: postPhotoLabel.image, withCaption: captionTextFiled.text, withCompletion: PFBooleanResultBlock?)
+    }
+
+       
     /*
      // MARK: - Navigation
      
